1c2bd0d4c5c1d478e02315e95a804fb6dc46b370
leonardoscampos1 <157980058+leonardoscampos1@users.noreply.github.com>
2025-07-25 17:07:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logging.basicConfig(
 )
 
 # Chave da API
-API_KEY = #'953c550f-fa61-461a-98d4-16671d4a4360-835686e9-0df7-4b35-a353-749dc2b19d7f'
+API_KEY = '1a18e6b7-c531-4335-bc58-281dbd02faaf-abd2b77a-73ca-45ae-ace2-e8d7bd13daf3'
 
 # Função para consultar o CNPJ com Simples Nacional
 def consultar_cnpj(cnpj):
